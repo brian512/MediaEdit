685c5402e1aea9aa98ee2a209574bec61554ef05
brian <huamm512@163.com>
2017-05-12 19:51:35 +0800

    add so

--- app/src/main/java/com/brian/mediaedit/MainActivity.java
+++ app/src/main/java/com/brian/mediaedit/MainActivity.java
@@ -2,6 +2,9 @@ package com.brian.mediaedit;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+
+import com.joyodream.recorder.jni.FFmpegCmd;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        FFmpegCmd.setEnableDebug(true);
+        final String folder = "/storage/emulated/0/DCIM";
+
+        FFmpegCmd.setPlaybackSpeed(folder + "/video.mp4", 2f, folder + "/video_new.mp4", new FFmpegCmd.OnCompletionListener() {
+            @Override
+            public void onCompletion(boolean result) {
+                Log.d("MainActivity", "result=" + result);
+            }
+        });
     }
 }
---
+++ ffmpegcmd/src/main/java/com/joyodream/recorder/FileUtil.java
@@ -0,0 +1,42 @@
+package com.joyodream.recorder;
+
+import android.text.TextUtils;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+
+/**
+ * Created by huamm on 2017/3/21 0021.
+ */
+
+public class FileUtil {
+
+    public static void writeFile(String path, String content, boolean append) {
+        FileWriter fileWriter;
+        try {
+            fileWriter = new FileWriter(path, append);
+            fileWriter.write(content);
+            fileWriter.flush();
+            fileWriter.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * 删除文件
+     */
+    public static boolean deleteFile(String path) {
+        if (TextUtils.isEmpty(path)) {
+            return false;
+        }
+        boolean ret = false;
+
+        File file = new File(path);
+        if (file.exists() && file.isFile()) {
+            ret = file.delete();
+        }
+        return ret;
+    }
+}
